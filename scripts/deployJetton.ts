09813d8b8b6b1034cca20fb34f97b76dac168222
Nikita Ivanov <nikita.i.dev@gmail.com>
2024-04-22 21:43:14 +0300

    another template

--- scripts/deployJetton.ts
+++ scripts/deployJetton.ts
@@ -3,19 +3,11 @@ import { Jetton } from '../wrappers/Jetton';
 import { compile, NetworkProvider } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const jetton = provider.open(
-        Jetton.createFromConfig(
-            {
-                id: Math.floor(Math.random() * 10000),
-                counter: 0,
-            },
-            await compile('Jetton')
-        )
-    );
+    const jetton = provider.open(Jetton.createFromConfig({}, await compile('Jetton')));
 
     await jetton.sendDeploy(provider.sender(), toNano('0.05'));
 
     await provider.waitForDeploy(jetton.address);
 
-    console.log('ID', await jetton.getID());
+    // run methods on `jetton`
 }
--- tests/Jetton.spec.ts
+++ tests/Jetton.spec.ts
@@ -18,15 +18,7 @@ describe('Jetton', () => {
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        jetton = blockchain.openContract(
-            Jetton.createFromConfig(
-                {
-                    id: 0,
-                    counter: 0,
-                },
-                code
-            )
-        );
+        jetton = blockchain.openContract(Jetton.createFromConfig({}, code));
 
         deployer = await blockchain.treasury('deployer');
 
@@ -43,39 +35,5 @@ describe('Jetton', () => {
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and jetton are ready to use
-    });
-
-    it('should increase counter', async () => {
-        const increaseTimes = 3;
-        for (let i = 0; i < increaseTimes; i++) {
-            console.log(`increase ${i + 1}/${increaseTimes}`);
-
-            const increaser = await blockchain.treasury('increaser' + i);
-
-            const counterBefore = await jetton.getCounter();
-
-            console.log('counter before increasing', counterBefore);
-
-            const increaseBy = Math.floor(Math.random() * 100);
-
-            console.log('increasing by', increaseBy);
-
-            const increaseResult = await jetton.sendIncrease(increaser.getSender(), {
-                increaseBy,
-                value: toNano('0.05'),
-            });
-
-            expect(increaseResult.transactions).toHaveTransaction({
-                from: increaser.address,
-                to: jetton.address,
-                success: true,
-            });
-
-            const counterAfter = await jetton.getCounter();
-
-            console.log('counter after increasing', counterAfter);
-
-            expect(counterAfter).toBe(counterBefore + increaseBy);
-        }
     });
 });
